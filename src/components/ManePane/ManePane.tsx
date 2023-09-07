51c5f4d3f140d35f9e16203d73f91339e49aa3ed
matthewbollington <mattbolly@yahoo.com>
2023-09-07 15:58:04 +0100

    layout-2 styling and moved sidebar and annotations

--- src/components/ManePane/ManePane.tsx
+++ src/components/ManePane/ManePane.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import styles from "./MainPane.module.css";
+import { useState } from "react";
 import useFetchData from "../../services/fetchData";
 import VideoCanvas from "../VideoCanvas/VideoCanvas";
+import { MainPaneProps } from "../../types/types";
 
-const MainPane: React.FC = () => {
+const MainPane: React.FC<MainPaneProps> = ({ setCurrentAnnotation }) => {
   const [frameRate, setFrameRate] = useState<number | null>(null);
   const { videoData, annotationData, error, loading } = useFetchData();
 
@@ -11,9 +11,10 @@ const MainPane: React.FC = () => {
   if (error) return <div>Error loading data: {error.message}</div>;
 
   return (
-    <div className={styles.mainPane} data-testid="mainPane">
+    <div className="mainPane" data-testid="mainPane">
       {videoData && annotationData && (
         <VideoCanvas
+          setCurrentAnnotation={setCurrentAnnotation}
           videoData={videoData}
           annotationData={annotationData}
           frameRate={frameRate}
--- src/components/Sidebar/tests/Sidebar.test.tsx
+++ src/components/Sidebar/tests/Sidebar.test.tsx
@@ -3,19 +3,19 @@ import Sidebar from "../Sidebar";
 
 describe("<Sidebar />", () => {
   it("renders without crashing", () => {
-    render(<Sidebar />);
+    render(<Sidebar currentAnnotation={null} />);
     const sidebarElement = screen.getByRole("complementary");
     expect(sidebarElement).toBeInTheDocument();
   });
 
   it("renders the correct number of video items", () => {
-    render(<Sidebar />);
+    render(<Sidebar currentAnnotation={null} />);
     const videoItems = screen.getAllByRole("listitem");
     expect(videoItems).toHaveLength(3);
   });
 
   it("renders each video with the correct text", () => {
-    render(<Sidebar />);
+    render(<Sidebar currentAnnotation={null} />);
     expect(screen.getByText("Labeyes video 1")).toBeInTheDocument();
     expect(screen.getByText("Labeyes video 2")).toBeInTheDocument();
     expect(screen.getByText("Labeyes video 3")).toBeInTheDocument();
